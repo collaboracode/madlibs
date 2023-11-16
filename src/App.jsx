5125f4c7abcc2a60cb8dd0cd07ab8cb5cca2e874
ahza64 <justinmichaelpettit@gmail.com>
2023-11-16 14:36:56 -0800

    broke up story into array before filtering blacklisted words and symbols. made api calls and stored word info object into an array with word indicies, so as to swap the word out in the array for the parOfSpeach (noun, verb, etc) into the story

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,43 @@ function App() {
 
   async function doStuff() {
     const q = await quotes.getQuote()
-    const filteredQ = dictionary.filterOutBlacklistedWords(q)
-    setQuote(filteredQ)
-    console.log("before: ", q);
-    console.log("after: ", filteredQ);
+    //split apart the string by words and every symbol and put into array
+    const seg = new Intl.Segmenter("en", { granularity: 'word' })
+    const quoteArray = [...seg.segment(q)].map(x => x.segment)
+    console.log(quoteArray);
+    // const filteredQ = dictionary.filterOutBlacklistedWords(q)
+    // setQuote(filteredQ)
+    // console.log("before: ", q);
+    // console.log("after: ", filteredQ);
+    async function findWords(numWords) {
+      const allWords = []
+      while (numWords > 0) {
+        //create a random number between 0 and story length
+        const randomInt = Math.floor(Math.random() * (quoteArray.length)); 
+        console.log("random int", randomInt);
+        //get word from array with randomInt
+        const randomWord = quoteArray[randomInt]
+        console.log("random word", randomWord);
+        //check if word 
+        const blacklistConditionals = ["", " ", ".", ",", "'", '"', "?", "/", "!", "a", "an", "and", "the", "it", "so", "is", "be", "to", "too", "am", "see", "on", "at", "or", "as", "in", "of"]
+        if (!blacklistConditionals.includes(randomWord)) {
+        //api call to see what type of word
+        const randomWordLowerCase = randomWord.toLowerCase()
+        const wordInfo = await dictionary.getWordInfo(randomWordLowerCase)
+        //keep track of index to be able to replace word with partOfSpeach insert
+        wordInfo.index = randomInt
+        allWords.push(wordInfo)
+        console.log("word info", wordInfo);
+        console.log("all words", allWords);
+        numWords = numWords - 1
+        }
+      }
+    }
+    findWords(4)
 
-    const arr = q.split(' ');
-    setWordArr(arr);
-    setWordCount(arr.length);
+    // const arr = q.split(' ');
+    // setWordArr(arr);
+    // setWordCount(arr.length);
   }
 
   useEffect(() => {
